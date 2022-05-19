73f5232fccaaab6af36a30768d2d4b43bfb1e87c
Yehudit-Brickner <nech.diti@yahoo.com>
2022-05-19 23:21:57 +0300

    started working on the iterators

--- sources/OrgChart.cpp
+++ sources/OrgChart.cpp
@@ -1,79 +1,79 @@
 
 
-#include "OrgChart.hpp"
-#include <unordered_map>
-#include <vector>
-using namespace std;
+// #include "OrgChart.hpp"
+// #include <unordered_map>
+// #include <vector>
+// using namespace std;
 
-using namespace ariel;
-
-
-int x =0;
-
-OrgChart::OrgChart(){
-this->_head=nullptr;
-}
-
-OrgChart OrgChart::add_root(string s){
-    // if(this->_head==nullptr){
-    //     this->_head=&s;
-    //     vector<string*> v;
-    //     this->_tree.insert(pair<string*,vector<string*>>(&s,v));
-    //     return *this;
-    // }
-    // else{
-    //    // find the row in the map change the key, or copy the vector, delete the row add in new row with new key old vector update _head
-    // }
-    return *this;
-}
+// using namespace ariel;
 
 
-OrgChart OrgChart::add_sub(string s1,string s2){
-    // if(this->_tree.find(&s1)==this->_tree.end()){ // will need to change to a different iterator
-    //     throw std::invalid_argument( "parent key not found" );
-    // }
-    // this->_tree[&s1].push_back(&s2);
-    // vector<string*> v;
-    // this->_tree.insert(pair<string*,vector<string*>>(&s2,v));
-    return *this;
-}
+// int x =0;
 
-ostream& ariel::operator<<(ostream& output, const OrgChart& org){
+// OrgChart::OrgChart(){
+// this->_head=nullptr;
+// }
+
+// OrgChart OrgChart::add_root(string s){
+//     // if(this->_head==nullptr){
+//     //     this->_head=&s;
+//     //     vector<string*> v;
+//     //     this->_tree.insert(pair<string*,vector<string*>>(&s,v));
+//     //     return *this;
+//     // }
+//     // else{
+//     //    // find the row in the map change the key, or copy the vector, delete the row add in new row with new key old vector update _head
+//     // }
+//     // return *this;
+// }
+
+
+// OrgChart OrgChart::add_sub(string s1,string s2){
+//     // if(this->_tree.find(&s1)==this->_tree.end()){ // will need to change to a different iterator
+//     //     throw std::invalid_argument( "parent key not found" );
+//     // }
+//     // this->_tree[&s1].push_back(&s2);
+//     // vector<string*> v;
+//     // this->_tree.insert(pair<string*,vector<string*>>(&s2,v));
+//     return *this;
+// }
+
+// ostream& ariel::operator<<(ostream& output, const OrgChart& org){
     
-    cout<< "printing OrgChart"<<endl; 
-    string s1="------------------";
-    string s2="---------";
-    string s3="|";
-    return output;
-}
+//     cout<< "printing OrgChart"<<endl; 
+//     string s1="------------------";
+//     string s2="---------";
+//     string s3="|";
+//     return output;
+// }
 
 
 
-string* OrgChart::begin_level_order(){
-    return nullptr;
-}
-string* OrgChart::end_level_order(){
-    return nullptr;
-}
-string* OrgChart::begin_reverse_order(){
-    return nullptr;
-}
-string* OrgChart::reverse_order(){
-    return nullptr;
-}
-string* OrgChart::begin_preorder(){
-    return nullptr;
-}
-string* OrgChart::end_preorder(){
-    return nullptr;
-}
+// string* OrgChart::begin_level_order(){
+//     return nullptr;
+// }
+// string* OrgChart::end_level_order(){
+//     return nullptr;
+// }
+// string* OrgChart::begin_reverse_order(){
+//     return nullptr;
+// }
+// string* OrgChart::reverse_order(){
+//     return nullptr;
+// }
+// string* OrgChart::begin_preorder(){
+//     return nullptr;
+// }
+// string* OrgChart::end_preorder(){
+//     return nullptr;
+// }
 
-int* OrgChart::begin(){
-    int* ans=&x;
-    return ans;
-}
+// int* OrgChart::begin(){
+//     int* ans=&x;
+//     return ans;
+// }
 
-int* OrgChart::end(){
-    int* ans=&x;
-    return ans;
-}
+// int* OrgChart::end(){
+//     int* ans=&x;
+// //     return ans;
+// }
--- sources/OrgChart.hpp
+++ sources/OrgChart.hpp
@@ -14,40 +14,87 @@ namespace ariel{
 
     class OrgChart{
         public:
-            string* _head;
-            unordered_map<string* , std::vector<string*>> _tree;
-            // data structer to hold the n tree
-            // map of tree key the node val, value vector of node sons
-            
-            OrgChart();
-            
-            OrgChart add_root(string s);
-            OrgChart add_sub(string s1,string s2);
-            friend ostream& operator<<(ostream& output, const OrgChart& org);
-            
-            string* begin_level_order();
-            string* end_level_order();
-            string* begin_reverse_order();
-            string* reverse_order();
-            string* begin_preorder();
-            string* end_preorder();
 
-            int* begin();
-            int* end();
+            struct Node{
+                string data;
+                vector<Node *> kids;
+                Node* parent;
+
+                Node(string &d) : data(d), parent(nullptr){}
+
+
+            };
+
+            Node* _root;
+
+            //constructer
+            OrgChart():_root(nullptr){}
+            
+            //create a "real root for the tree or change the string of the root
+            OrgChart add_root(string s){
+                if (this->_root==nullptr){
+                    this->_root=new Node(s);
+                }
+                else{
+                    this->_root->data=s;
+                }
+                return *this;
+            }
+
+
+            // add a kid to one of the nodes in the tree
+            // we will find the parent node and add the kid to its vector
+            OrgChart add_sub(string s1,string s2){
+                if (this->_root==nullptr){
+                    // throw an error
+                }
+                for (auto it = *this.begin_level_order(); it != *this.end_level_order(); ++it){
+                    if (it->data==s1){
+                        it->kids.add(new Node(s2));
+                        return *this;
+                    }
+                }
+                // throw error parent no in orgchart
+                
+            }
+
+           
+
+            // print the tree in a nice way
+            friend ostream& operator<<(ostream& output, const OrgChart& org){
+                cout<< "printing OrgChart"<<endl; 
+                string s1="------------------";
+                string s2="---------";
+                string s3="|";
+                return output;
+            }
+            
+           
             
             class level_order_iterator{
                 private:
-                    queue<string*> q;
-                    string* pointer_to_current_node;
-
+                    queue<Node*> q;
+                    Node* pointer_to_current_node;
                     
                 public:
-                    level_order_iterator(){
-                       
+                    level_order_iterator(Node* n){
+                        queue<Node*>helper;
+                        helper.push(n);
+                        while(helper.size()!=0){
+                            Node* dad=helper.front();
+                            this->q.push(dad);
+                            helper.pop();
+                            getkids(dad, &helper); 
+                        }
+                       pointer_to_current_node = n;
                     }
 
-                    level_order_iterator(string* ptr = nullptr): pointer_to_current_node(ptr) {
-		            }  
+                    void getkids(Node* n, queue<Node*>* que){
+                        for (unsigned long i =0; i< n->kids.size();i++){
+                            que->push(n->kids[i]);
+                        } 
+                    }
+ 
 
                     level_order_iterator& operator*(){
                        return *this; 
@@ -57,21 +104,20 @@ namespace ariel{
                         return this;
                     }
 
-                    level_order_iterator& operator++();
-                    // {
-                    //     pointer_to_current_node = pointer_to_current_node++;
-			        //     return *this;
-                    // }
+                    level_order_iterator& operator++(){
+                        this->q.pop();
+                        pointer_to_current_node=q.front();
+			            return *this;
+                    }
 
-                    level_order_iterator& operator++(int);
-                    // {
-                    //     level_order_iterator iterator=*this;
-                    //     ++(*this);
-                    //     return iterator; 
-                    // }
+                    level_order_iterator& operator++(int){
+                        level_order_iterator iterator=*this;
+                        ++(*this);
+                        return iterator; 
+                    }
 
 
-                     bool operator==(const level_order_iterator& other) const{
+                    bool operator==(const level_order_iterator& other) const{
                         return pointer_to_current_node == other.pointer_to_current_node;
                     }
 
@@ -79,22 +125,19 @@ namespace ariel{
                         return !(*this==other);
                     }
                  
-                    level_order_iterator begin_level_order(){
-                        return pointer_to_current_node;
-                    }
-                    level_order_iterator end_level_order(){
-                        return nullptr;
-                    }
             };
 
 
-    class reverse_level_order_iterator{
+            class reverse_level_order_iterator{
                 private:
-                    string* pointer_to_current_node;
+                    Node* pointer_to_current_node;
+                    queue<Node*> q;
                     
                 public:
-                    reverse_level_order_iterator(string* ptr = nullptr): pointer_to_current_node(ptr) {
-		            }  
+                    reverse_level_order_iterator(Node* n){
+                        
+
+                    } 
 
                     reverse_level_order_iterator& operator*(){
                        return *this; 
@@ -126,22 +169,32 @@ namespace ariel{
                         return !(*this==other);
                     }
                  
-                    reverse_level_order_iterator begin_reverse_order(){
-                        return pointer_to_current_node;
-                    }
-                    reverse_level_order_iterator reverse_order(){
-                        return nullptr;
-                    }
             };
 
             class preorder_iterator{
                 private:
-                    string* pointer_to_current_node;
+                    queue<Node*> q;
+                    Node* pointer_to_current_node;
                     
                 public:
-                    preorder_iterator(string* ptr = nullptr): pointer_to_current_node(ptr) {
-		            }  
+                   
+                    preorder_iterator(Node* n){
+                        stack<Node*> helper;
+                        helper.push(n);
+                        while(helper.size()!=0){
+                            Node* dad=helper.top();
+                            this->q.push(dad);
+                            getkids(dad, &helper);
+                        }
+                        pointer_to_current_node = n;
+                    }
 
+                    void getkids(Node* n, stack<Node*>* st){
+                        for (unsigned long i =n->kids.size()-1; i>=0;i--){
+                            st->push(n->kids[i]);
+                        } 
+                    }
+                    
                     preorder_iterator& operator*(){
                        return *this; 
                     } 
@@ -150,18 +203,17 @@ namespace ariel{
                         return this;
                     }
 
-                    reverse_level_order_iterator& operator++();
-                    // {
-                    //     pointer_to_current_node = pointer_to_current_node++;
-			        //     return *this;
-                    // }
+                    preorder_iterator& operator++(){
+                        this->q.pop();
+                        pointer_to_current_node=this->q.front();
+			            return *this;
+                    }
 
-                    reverse_level_order_iterator& operator++(int);
-                    // {
-                    //     level_order_iterator iterator=*this;
-                    //     ++(*this);
-                    //     return iterator; 
-                    // }
+                    preorder_iterator& operator++(int){
+                        preorder_iterator iterator=*this;
+                        ++(*this);
+                        return iterator; 
+                    }
 
 
                      bool operator==(const preorder_iterator& other) const{
@@ -171,72 +223,39 @@ namespace ariel{
                     bool operator!=(const preorder_iterator& other) const{
                         return !(*this==other);
                     }
-                 
-                    preorder_iterator begin_preorder(){
-                        return pointer_to_current_node;
-                    }
-                    preorder_iterator end_preorder(){
-                        return nullptr;
-                    }
             };
+
+            level_order_iterator begin_level_order(){
+               return level_order_iterator(this->_root);
+            }
+            level_order_iterator end_level_order(){
+                Node* n =nullptr;
+                return level_order_iterator(n);
+            }
+
+            reverse_level_order_iterator begin_reverse_order(){ 
+                return reverse_level_order_iterator(this->_root);
+            }
+            reverse_level_order_iterator reverse_order(){
+                Node* n =nullptr;
+                return reverse_level_order_iterator(n); 
+            }
+
+            preorder_iterator begin_preorder(){
+                return preorder_iterator(this->_root);
+            }
+            preorder_iterator end_preorder(){
+                Node* n =nullptr;
+                return preorder_iterator(n);
+            }
+
+
+
+            // int* begin();
+            // int* end();
+
     };
             
 }
 
-    // template<typename Vector>
-    // class VectorIterator{
-    //     public:
-    //         using ValueType = typename Vector::ValueType;
-    //         using PointerType = ValueType*;
-    //         using ReferenceType = valueTyoe&;
-    //     public:
-    //         VectorIterator(PointerType ptr)
-    //             : m_Ptr(ptr){}
-
-    //         VectorIterator& operatpr++(){
-    //             m_Ptr++;
-    //             return *this;
-    //         }
-
-    //          VectorIterator& operatpr++(int){
-    //              VectorIterator iterator=*this;
-    //              ++(*this);
-    //              return iterator; 
-    //         }
-
-    //          VectorIterator& operatpr--(){
-    //             m_Ptr--;
-    //             return *this;
-    //         }
-
-    //          VectorIterator& operatpr--(int){
-    //              VectorIterator iterator=*this;
-    //              --(*this);
-    //              return iterator; 
-    //         }
-
-    //         ReferenceType operator[](int index){
-    //             return *(m_ptr + index)
-    //         }
-
-
-    //         PointerType operator->(){
-    //             return m_ptr;
-    //         }
-
-    //         ReferenceType operator*(){
-    //             return *m_Ptr;
-    //         }
-
-    //         bool operator==(const VectorIterator& other) const{
-    //             return m_Ptr == other.m_Ptr;
-    //         }
-
-    //         bool operator!=(const VectorIterator& other) const{
-    //             return I(*this==other);
-    //         }
-
-    //     private:
-    //         PointerType m_ptr;
-    // };
-
+  
